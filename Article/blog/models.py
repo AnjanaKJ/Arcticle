34de57206dbada28d5964f3e2e7098fc2009b98d
AnjanaKJ <96719400+AnjanaKJ@users.noreply.github.com>
2023-03-08 17:26:57 +0530

    Update models.py

--- Article/blog/models.py
+++ Article/blog/models.py
@@ -5,7 +5,6 @@ from django.urls import reverse
 class Article(models.Model):
     name=models.CharField(max_length=25,blank=True,null=True)
     email = models.EmailField(default="abcd@gmail.com")
-    price = models.DecimalField(decimal_places=2,max_digits=1000000)
     des = models.TextField(default="Write article .",null = False)
 
 def get_absolute_url(self):
